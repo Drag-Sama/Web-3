5998387255051949fac506a70a8dcbfe315b17a9
Drag-Sama <dragsama6@gmail.com>
2025-04-28 15:59:41 +0200

    On avance

--- tvshow/tvshow.sql
+++ tvshow/tvshow.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 28 avr. 2025 à 14:55
+-- Généré le : lun. 28 avr. 2025 à 15:59
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -61,13 +61,16 @@ CREATE TABLE `contient` (
 --
 
 INSERT INTO `contient` (`titre_saison`, `id_episode`) VALUES
-('Saison 1 Malcom', 1),
-('Saison 1 Malcom', 2),
-('Saison 1 Malcom', 3),
-('Saison 1 Malcom', 4),
-('Saison 1 Malcom', 5),
+('Saison 1 Malcolm', 1),
+('Saison 1 Malcolm', 2),
+('Saison 1 Malcolm', 3),
+('Saison 1 Malcolm', 4),
+('Saison 1 Malcolm', 5),
+('Saison 1 Breaking Bad', 6),
 ('Saison 1 Breaking Bad', 7),
-('Saison 1 Breaking Bad', 6);
+('Saison 1 Breaking Bad', 8),
+('Saison 1 Breaking Bad', 9),
+('Saison 1 Breaking Bad', 10);
 
 -- --------------------------------------------------------
 
@@ -93,7 +96,10 @@ INSERT INTO `episode` (`titre`, `desc`, `duree`, `id_Episode`) VALUES
 ('Honte', 'Malcolm est rongé par le remords quand il apprend qu’il a frappé un enfant de sept ans.', 23, 4),
 ('Changement de famille', 'Malcolm est engagé comme baby-sitter dans une famille riche avec une somptueuse villa.', 23, 5),
 ('Chute libre', 'Professeur de chimie dans un lycée, Walter White travaille parallèlement dans une station de lavage de voitures afin de boucler les fins de mois de sa famille, composée de sa femme Skyler, qui est enceinte, et de son fils Walter Jr, un adolescent handicapé. Alors qu\'il découvre qu\'il est atteint d\'un cancer du poumon en stade terminal, Walter décide de reprendre sa vie en main. Utilisant ses connaissances en chimie, il s\'attelle à la fabrication de méthamphétamines, avec l\'aide de Jesse Pinkman, un ancien élève qui s\'est spécialisé dans le trafic de drogues...', 58, 6),
-('Le choix', 'Après leur mésaventure dans le désert, Walter et Pinkman doivent se débarrasser des corps des deux trafiquants qu\'ils ont éliminé. Leur tâche se complique quand ils découvrent avec stupéfaction que l\'un d\'entre eux a survécu. Les deux acolytes ne sont pas d\'accord quant à la marche à suivre. De son côté, Skyler s\'interroge sur le comportement étrange de son mari. Intriguée par un appel, elle mène l\'enquête pour découvrir ce qu\'il fabrique. Au lycée, Walter ne parvient pas à faire abstraction de ses ennuis...', 42, 7);
+('Le choix', 'Après leur mésaventure dans le désert, Walter et Pinkman doivent se débarrasser des corps des deux trafiquants qu\'ils ont éliminé. Leur tâche se complique quand ils découvrent avec stupéfaction que l\'un d\'entre eux a survécu. Les deux acolytes ne sont pas d\'accord quant à la marche à suivre. De son côté, Skyler s\'interroge sur le comportement étrange de son mari. Intriguée par un appel, elle mène l\'enquête pour découvrir ce qu\'il fabrique. Au lycée, Walter ne parvient pas à faire abstraction de ses ennuis...', 42, 7),
+('Dérapage', 'Walter n\'a toujours pas effectué sa part du marché conclu avec Jesse qui le presse d\'en finir. Il tergiverse et hésite à commettre un acte qui pourrait le hanter toute sa vie et mettre sa famille en danger. Alors qu\'elle peint la chambre du bébé avec sa soeur, Skyler lui raconte qu\'elle écrit un récit sur un personnage drogué et lui pose des questions sur la marijuana.', 42, 8),
+('Retour aux sources', 'Hank informe ses troupes que de nouveaux dealers opèrent à Albuquerque, proposant une drogue de très haute qualité. Lors d\'un barbecue en famille chez les White, Skyler explose en sanglots, forçant Walt à révéler à sa famille qu\'il est atteint d\'un cancer des poumons. Marie propose de faire jouer ses relations pour lui obtenir un rendez-vous chez un oncologue de renom.', 42, 9),
+('Vivre ou survivre', 'Jesse tente d\'obtenir un travail régulier mais il constate rapidement que sans qualifications et sans expérience, ce n\'est pas chose facile. Il se tourne donc à nouveau vers la fabrication de drogue avec l\'aide d\'un ancien ami. Toutefois, sans les connaissances de Walt, il ne parvient pas à obtenir la qualité voulue.\r\nWalt, lui, revoit un ancien collègue, aujourd\'hui très riche, qui lui propose un travail et de payer son traitement.', 42, 10);
 
 -- --------------------------------------------------------
 
@@ -111,14 +117,14 @@ CREATE TABLE `joue` (
 --
 
 INSERT INTO `joue` (`titre_saison`, `nom_acteur`) VALUES
-('Saison 1 Malcom', 'Bryan Cranston'),
-('Saison 1 Malcom', 'Christopher Masterson'),
-('Saison 1 Malcom', 'Erik Per Sullivan'),
-('Saison 1 Malcom', 'Frankie Muniz'),
-('Saison 1 Malcom', 'Jane Kaczmarek'),
-('Saison 1 Malcom', 'Justin Berfied'),
-('Saison 1 Breaking Bad', 'Bryan Cranston'),
-('Saison 1 Breaking Bad', 'Aaron Paul');
+('Saison 1 Malcolm', 'Jane Kaczmarek'),
+('Saison 1 Malcolm', 'Bryan Cranston'),
+('Saison 1 Malcolm', 'Justin Berfied'),
+('Saison 1 Malcolm', 'Frankie Muniz'),
+('Saison 1 Malcolm', 'Erik Per Sullivan'),
+('Saison 1 Malcolm', 'Christopher Masterson'),
+('Saison 1 Breaking Bad', 'Aaron Paul'),
+('Saison 1 Breaking Bad', 'Bryan Cranston');
 
 -- --------------------------------------------------------
 
@@ -140,8 +146,10 @@ INSERT INTO `realisateur` (`nom`, `photo`) VALUES
 ('Andrew Orenstein', 'andrew_orenstein.png'),
 ('Arlene Sanford', 'arlene_sanford.jpg'),
 ('Jeff Melman', 'jeff_melman.jpg'),
+('Jim McKay', 'jim_mckay.jpg'),
 ('Nick Marck', 'nick_marck.jpg'),
 ('Todd Holland', 'todd_holland.png'),
+('Tricia Borck.jpg', 'tricia_borck.jpg'),
 ('Vince Gilligan', 'vince_gilligan.jpg');
 
 -- --------------------------------------------------------
@@ -167,7 +175,10 @@ INSERT INTO `realise` (`nom_real`, `id_episode`) VALUES
 ('Nick Marck', 4),
 ('Jeff Melman', 5),
 ('Vince Gilligan', 6),
-('Adam Bernstein', 7);
+('Adam Bernstein', 7),
+('Adam Bernstein', 8),
+('Jim McKay', 9),
+('Tricia Borck.jpg', 10);
 
 -- --------------------------------------------------------
 
@@ -176,19 +187,19 @@ INSERT INTO `realise` (`nom_real`, `id_episode`) VALUES
 --
 
 CREATE TABLE `saison` (
-  `num_saison` int(255) NOT NULL,
-  `affiche` text NOT NULL,
+  `titre` varchar(255) NOT NULL,
   `titre_serie` varchar(255) NOT NULL,
-  `titre` varchar(255) NOT NULL
+  `affiche` text NOT NULL,
+  `descr` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `saison`
 --
 
-INSERT INTO `saison` (`num_saison`, `affiche`, `titre_serie`, `titre`) VALUES
-(1, 'breaking_bad_saison1.jpg', 'Breaking Bad', 'Saison 1 Breaking Bad'),
-(1, 'malcom_saison1.jpg', 'Malcom', 'Saison 1 Malcom');
+INSERT INTO `saison` (`titre`, `titre_serie`, `affiche`, `descr`) VALUES
+('Saison 1 Breaking Bad', 'Breaking Bad', 'breaking_bad_saison1.jpg', 'Walter White, 50 ans, est professeur de chimie dans un lycée du Nouveau-Mexique. Son quotidien est bouleversé lorsqu’il apprend qu’il est atteint d’un cancer des poumons incurable. Il n’a plus qu’une seule solution pour mettre rapidement sa famille à l’abri du besoin : fabriquer et vendre une drogue de synthèse. Commence alors une longue descente aux enfers.'),
+('Saison 1 Malcolm', 'Malcolm', 'malcolm_saison1.jpg', 'Petit génie malgré lui, Malcolm vit dans une famille hors du commun. Le jeune surdoué n\'hésite pas à se servir de son intelligence pour faire les 400 coups avec ses frères : Francis, l\'aîné, envoyé dans une école militaire après une bêtise de trop, Reese, une brute pas très maligne, et Dewey, le petit dernier, souffre douleur général. Les parents tentent tant bien que mal de canaliser l\'énergie de ces petits démons. Si Loïs est despotique et veut tout contrôler, Hal, en revanche, est irresponsable et ne pense qu\'à s\'amuser.');
 
 -- --------------------------------------------------------
 
@@ -197,7 +208,7 @@ INSERT INTO `saison` (`num_saison`, `affiche`, `titre_serie`, `titre`) VALUES
 --
 
 CREATE TABLE `serie` (
-  `titre_serie` varchar(255) NOT NULL,
+  `titre` varchar(255) NOT NULL,
   `tag` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -205,9 +216,9 @@ CREATE TABLE `serie` (
 -- Déchargement des données de la table `serie`
 --
 
-INSERT INTO `serie` (`titre_serie`, `tag`) VALUES
+INSERT INTO `serie` (`titre`, `tag`) VALUES
 ('Breaking Bad', 'drame'),
-('Malcom', 'humour');
+('Malcolm', 'humour');
 
 -- --------------------------------------------------------
 
@@ -282,7 +293,7 @@ ALTER TABLE `saison`
 -- Index pour la table `serie`
 --
 ALTER TABLE `serie`
-  ADD PRIMARY KEY (`titre_serie`),
+  ADD PRIMARY KEY (`titre`),
   ADD KEY `tag_fk_key` (`tag`);
 
 --
@@ -320,7 +331,7 @@ ALTER TABLE `realise`
 -- Contraintes pour la table `saison`
 --
 ALTER TABLE `saison`
-  ADD CONSTRAINT `ser_fk_key` FOREIGN KEY (`titre_serie`) REFERENCES `serie` (`titre_serie`);
+  ADD CONSTRAINT `ser_fk_key` FOREIGN KEY (`titre_serie`) REFERENCES `serie` (`titre`);
 
 --
 -- Contraintes pour la table `serie`
