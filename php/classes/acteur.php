8fa7b91dbbcd915db727eb80e183b43b63f23087
HOMIES-HUB <qv62100@gmail.com>
2025-04-28 15:46:42 +0200

    d

---
+++ php/classes/acteur.php
@@ -0,0 +1,13 @@
+<?php
+Class Acteur {
+    private string $nom;
+    private string $photo;
+
+    public function __construct($nom_act,$nom_photo) {
+        $nom = $nom_act;
+        $photo = $nom_photo;
+    }
+    
+}
+
+?>
--- php/researchSerie.php
+++ php/researchSerie.php
@@ -1,15 +1,18 @@
 <?php
-$text = /*$_GET["text"];*/ "Bryan";
-include "connectDB.php";
-$sql = "SELECT DISTINCT serie.titre_serie FROM serie INNER JOIN saison INNER JOIN contient INNER JOIN episode INNER JOIN realise INNER JOIN joue WHERE nom_acteur LIKE '%" . $text . "%';";
+$text = $_GET["text"];
+require "connectDB.php";
+$sql = "SELECT DISTINCT serie.titre,affiche FROM serie INNER JOIN saison INNER JOIN contient INNER JOIN episode INNER JOIN realise INNER JOIN joue WHERE nom_acteur LIKE '%" . $text . "%' OR serie.titre LIKE '%" . $text . "%' OR nom_real LIKE '%" . $text . "%'
+OR tag LIKE '%" . $text . "%';" ; // renvoie toutes les séries où le texte recherché se trouve dans le titre de la série / le nom d'un acteur ou réalisateur / nom d'un tag.
 $statement = $pdo->prepare($sql);
 $statement->execute() or die(var_dump($statement->errorInfo()));
-$results = $statement->fetchAll(PDO::FETCH_OBJ) ; ?>
+$results = $statement->fetchAll(PDO::FETCH_OBJ); 
 
-Série : <?php 
-    foreach ($results as $serie):
-    ?>
-    <li><?= $serie->titre ?></li>
-<?php
-    endforeach;
+foreach ($results as $serie):
+    echo(
+        "<div>
+            <img url='../photos/saison/$serie->affiche'/>
+            <p>$serie->titre</p>
+        </div>"
+    );
+endforeach;
 ?>
