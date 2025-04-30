7d85ee8521fe3175b81b87b40cabf594352155e8
HOMIES-HUB <qv62100@gmail.com>
2025-04-30 13:46:11 +0200

    d

--- homepage.php
+++ homepage.php
@@ -2,9 +2,16 @@
 
 <?php require "php/classes/template.php";
 require "php/researchSerie.php";
+require "php/classes/BD.php";
 ob_start();
 
 ?>
+    <form action="/choiceTag.php">
+        <label for="tag">Tags :</label>
+        <select name="tag">
+
+        </select>
+    </form>
     <script src="filterTags.js"></script>
     <div class="cartes">
     <?php researchSerie(); ?>
