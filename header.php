3a11673977387641e0319ef348af0e0adde2fc95
Drag-Sama <dragsama6@gmail.com>
2025-05-05 14:37:21 +0200

    header

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <header class="header-titre">
-            <a class="titre" href="homepage.php">Tv Show</a>
+            <a class="titre" href="homepage.php">QWERTZ video</a>
         <form class="recherche" action="homepage.php?text=$_GET['text']" method="get">
             <input type="text" name="text" class="search-input" placeholder="Rechercher une serie..">
             <button type="submit" class="btn btn-primary">Click</button>
