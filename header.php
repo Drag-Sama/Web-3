d1019c51b78f7695170f68bbdd1bb87da60f732a
Drag-Sama <dragsama6@gmail.com>
2025-05-05 09:04:08 +0200

    css mieux

--- header.php
+++ header.php
@@ -1,1 +1,7 @@
-<h1 class="titre">TV Shows</h1>
+<header class="header-titre">
+        <div class="titre">Tv Show</div>
+        <form class="recherche" action="homepage.php?text=$_GET['text']" method="get">
+            <input type="text" name="text" class="search-input" placeholder="Rechercher une serie..">
+            <button type="submit" class="btn btn-primary">Click</button>
+        </form>
+    </header>
