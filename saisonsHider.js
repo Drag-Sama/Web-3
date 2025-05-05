42e2c0882f546da572b3f4fe4e1eeef09690bbea
HOMIES-HUB <qv62100@gmail.com>
2025-05-05 13:30:14 +0200

    d

--- saisonsHider.js
+++ saisonsHider.js
@@ -1,5 +1,14 @@
 
-saisonSelector = document.getElementById("saisons");
-saisonSelector.addEventListener("change", function(e) {
-    
-}) 
+
+
+function saisonsHider() {
+    saisonSelector = document.getElementById("saisons");
+    n_saison = saisonSelector.value; // num de la saison voulue.
+    saisonContainers = document.getElementsByClassName("saison-container");
+    i = 1;
+    for (const container of saisonContainers) {
+        if (i != n_saison) container.style.display = "none";
+        else container.style.display = "block";
+        i++;
+    }
+}
