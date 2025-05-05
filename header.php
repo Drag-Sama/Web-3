fc61dccd989b14a81d2104f92d1623bfad848fc6
Drag-Sama <dragsama6@gmail.com>
2025-05-05 11:46:47 +0200

    neu neu neu neu neu

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
-<a class="header-titre">
-            <div class="titre" onclick="header(Location: homepage.php">Tv Show</div>
+<header class="header-titre">
+            <div class="titre">Tv Show</div>
         <form class="recherche" action="homepage.php?text=$_GET['text']" method="get">
             <input type="text" name="text" class="search-input" placeholder="Rechercher une serie..">
             <button type="submit" class="btn btn-primary">Click</button>
