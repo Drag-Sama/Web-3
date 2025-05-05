c92e366e730c70052906f72825ee1d8bffdf991c
Drag-Sama <dragsama6@gmail.com>
2025-05-05 13:36:28 +0200

    value

--- php/displaySaisons.php
+++ php/displaySaisons.php
@@ -5,7 +5,7 @@ function displaySelectSaisons(BD $BD) {
         $saisons = $BD->get_saison_serie($serie);
         echo "<select name='saisons' id='saisons' onchange='saisonsHider()'>";
         foreach ($saisons as $saison):
-            echo "<option value='Saison $saison->num_saison'>Saison $saison->num_saison</option>";
+            echo "<option value='$saison->num_saison'>Saison $saison->num_saison</option>";
         endforeach;
         echo "</select>";
     }
@@ -16,8 +16,8 @@ function displaySaisons(BD $BD) {
         $titre = $_GET['serie'];
         $saisons = $BD->get_saison_serie($titre);
         foreach ($saisons as $saison):
-            echo "<img class='saison-image' src='/web3/Web-3/photos/saison/$saison->affiche'/>";
             echo"<div class = 'saison-container' id = '$saison->num_saison'>";
+            echo "<img class='saison-image' src='/web3/Web-3/photos/saison/$saison->affiche'/>";
             echo"<div class='saison-title'>$saison->titre</div>";
             echo "<div class='saison-description'> $saison->descr </div>";
             echo"</div>";
