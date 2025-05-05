ad172695bffef72afb48e3435f2dbaf3ccbba2cb
HOMIES-HUB <qv62100@gmail.com>
2025-05-05 11:54:29 +0200

    Update displaySaisons.php

--- php/displaySaisons.php
+++ php/displaySaisons.php
@@ -3,9 +3,11 @@ function displaySaisons(BD $BD) {
     if (isset($_GET['serie'])) {
         $serie = $_GET['serie'];
         $saisons = $BD->get_saison_serie($serie);
-        echo $saisons;
-        echo $saisons[0];
-        echo "fortnite";
+        echo "<select id = 'saisons'>"
+        foreach ($saisons as $saison):
+            echo
+        endforeach;
+        echo 
     }
 }
 ?>
