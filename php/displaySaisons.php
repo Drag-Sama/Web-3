befbd1592917d46b98566e2e5165b85180482aae
Drag-Sama <dragsama6@gmail.com>
2025-05-05 13:34:39 +0200

    /

--- php/displaySaisons.php
+++ php/displaySaisons.php
@@ -16,7 +16,7 @@ function displaySaisons(BD $BD) {
         $titre = $_GET['serie'];
         $saisons = $BD->get_saison_serie($titre);
         foreach ($saisons as $saison):
-            echo "<img class='saison-image' src='/web3/Web-3/photos/saison/$saison->affiche'";
+            echo "<img class='saison-image' src='/web3/Web-3/photos/saison/$saison->affiche'/>";
             echo"<div class = 'saison-container' id = '$saison->num_saison'>";
             echo"<div class='saison-title'>$saison->titre</div>";
             echo "<div class='saison-description'> $saison->descr </div>";
