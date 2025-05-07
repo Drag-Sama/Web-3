97312aac7b4081788992d3d2f558e900cfa3d040
Drag-Sama <dragsama6@gmail.com>
2025-05-07 08:53:34 +0200

    dazdza:fgtzeyuc

--- showpage.php
+++ showpage.php
@@ -43,14 +43,11 @@ ob_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Show Page</title>
-
     <link rel="stylesheet" href="showpage.css">
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
     crossorigin="anonymous">
     <link rel="stylesheet" href="showpage.css">
-
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" 
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" 
         crossorigin="anonymous"></script>
@@ -69,11 +66,15 @@ ob_start();
             
             <div class="episodes-list">
             
-               
+
             </div>
             <div class="actors-section">
+                <div class="actor">
                 <div class="cartes">
                     <?php $displayer->displayActeur(); ?>
+
+                    <img class="actor-photo" src="">
+                    <div class="actor-name">Nom de l'acteur </div>
                 </div>
             </div>
         </div>
